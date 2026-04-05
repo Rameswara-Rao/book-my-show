13d54d109a0428af03adc0ff5312ab0466ac72ff
Rameswara Rao D <rameswararaod@Rameswaras-MacBook-Pro.local>
2026-04-06 00:06:15 +0530

    feat: initial MERN BookMyShow project setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config(); // Load environment variables
+
+const connectDB = require('./config/db'); // Import database configuration
+const userRouter = require('./routes/userRoute'); // Import user routes
+
+connectDB(); // Connect to database
+
+/** Routes */
+app.use(express.json());
+app.use('/api/users', userRouter);
+
+app.listen(8082, () => {
+  console.log('Server is Running');
+});
